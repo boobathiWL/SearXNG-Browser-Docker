36c964c09c6ac25e38fefccd71b14e369222570b
boobathiWL <boobathi@wl.team>
2024-09-27 11:41:17 +0530

    first

---
+++ searx/version_frozen.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# pylint: disable=missing-module-docstring
+# this file is generated automatically by searx/version.py
+
+VERSION_STRING = "2024.9.23+6be56aee1"
+VERSION_TAG = "2024.9.23+6be56aee1"
+DOCKER_TAG = "2024.9.23-6be56aee1"
+GIT_URL = "https://github.com/searxng/searxng"
+GIT_BRANCH = "master"
